2414f191ef8b6bf2b097c2dfc6b23e9a5c507ab9
fvilmot <fvilmot@gmail.com>
2024-04-11 10:22:09 +0200

    addi infinite scroll

--- src/app/components/pictures-display/pictures-display.component.ts
+++ src/app/components/pictures-display/pictures-display.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
 
@@ -10,7 +15,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './pictures-display.component.scss',
 })
 export class PicturesDisplayComponent {
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private elementRef: ElementRef,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   photos: any[] = [];
   query: string = 'random';
@@ -21,16 +30,45 @@ export class PicturesDisplayComponent {
     this.getPhotos();
   }
 
+  onPhotosLoaded() {
+    this.cdr.detectChanges();
+
+    this.checkIfLastLiIsVisible();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  @HostListener('window:resize', ['$event'])
+  onWindowEvent(): void {
+    this.checkIfLastLiIsVisible();
+  }
+
+  checkIfLastLiIsVisible(): void {
+    const lastLiElement =
+      this.elementRef.nativeElement.querySelector('ul li:last-child');
+    if (lastLiElement) {
+      const lastLiPosition = lastLiElement.getBoundingClientRect().bottom;
+      const windowHeight = window.innerHeight;
+
+      if (lastLiPosition <= windowHeight) {
+        this.loadMore();
+      }
+    }
+  }
+
   getPhotos() {
     this.apiService
       .getPhotos(this.query, this.pageNumber)
       .subscribe((data: any) => {
         this.photos = [...this.photos, ...data.results];
         this.maxPages = data.total_pages;
+        // console.log(data.results);
+        this.onPhotosLoaded();
       });
   }
 
   loadMore() {
+    console.log(`${this.pageNumber}/${this.maxPages}`);
+
     if (this.pageNumber < this.maxPages) {
       this.pageNumber++;
       this.getPhotos();
